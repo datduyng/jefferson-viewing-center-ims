c7ba8d018c9bc88adabea755ae174e013192f823
datduyng <datnguyen5653@huskers.unl.edu>
2018-05-28 23:11:04 -0500

    added back productType and changed to static

--- assignment/src/ims/InvoiceReport.java
+++ assignment/src/ims/InvoiceReport.java
@@ -4,10 +4,14 @@ package ims;
 public class InvoiceReport {
 	
 	public static void main(String[] args) {
+		
+		// read and process data from file.
 		DataConverter.readPersonFile();
 		DataConverter.readCustomerFile();
 		DataConverter.readProductFile();
 		DataConverter.readInvoiceFile();
+		
+		// TODO: print 2 report. 
 	}// end main()
 }// end InvoiceReport class
 
--- assignment/src/ims/Product.java
+++ assignment/src/ims/Product.java
@@ -4,17 +4,25 @@ package ims;
 public class Product {
 
 	private String productCode;
+	private static String productType; 
+	
 	public Product() {
 		
 	}
 	
 	public Product(String productCode, String productType) {
 		setProductCode(productCode);
+		setProductType(productType);
 	}
 
 	public String getProductCode() {
 		return productCode;
 	}
+	
+	public String getProductType() {
+		return productType;
+	}
+
 
 	
 	public void setProductCode(String productCode) {
@@ -25,13 +33,21 @@ public class Product {
 		}
 		
 	}
+	
+	public void setProductType(String productType) {
+		if(productType != null && productType.length() > 0) {
+			Product.productType = productType;
+		}else {
+			Product.productType = "na";
+		}
+	}
 
 
 	/**
 	 * @Override
 	 */
 	public String toString() {
-		return this.productCode;
+		return this.productCode + ";" + Product.productType;
 	}
 	
 	
