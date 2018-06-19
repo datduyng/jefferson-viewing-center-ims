054fedaebba679028f657df06c250cab8978e969
Dat Nguyen <35666615+datduyng@users.noreply.github.com>
2018-06-19 11:41:19 -0500

    added extra script

---
+++ assignment/assignment04/deleteImsTable.sql
@@ -0,0 +1,145 @@
+
+DROP TABLE IF EXISTS `InvoiceProducts`;
+DROP TABLE IF EXISTS `Invoices`;
+DROP TABLE IF EXISTS `Refreshments`;
+DROP TABLE IF EXISTS `ParkingPasses`;
+DROP TABLE IF EXISTS `SeasonPasses`;
+DROP TABLE IF EXISTS `MovieTickets`;
+DROP TABLE IF EXISTS `Products`;
+DROP TABLE IF EXISTS `Customers`;
+DROP TABLE IF EXISTS `Emails`;
+DROP TABLE IF EXISTS `Persons`;
+DROP TABLE IF EXISTS `Addresses`;
+DROP TABLE IF EXISTS `StateCountries`;
+DROP TABLE IF EXISTS `Countries`;
+
+
+
+
+CREATE TABLE Products(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+productCode VARCHAR(255) UNIQUE NOT NULL,
+productType VARCHAR(2) NOT NULL
+);
+
+
+CREATE TABLE Refreshments(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+cost float NOT NULL default '0.0',
+haveTicket boolean NOT NULL default 0,# TODO:
+productID INT NOT NULL,
+FOREIGN KEY(productID) REFERENCES Products(id)
+);
+
+CREATE TABLE ParkingPasses(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+fee float NOT NULL DEFAULT '0.0',
+productID INT NOT NULL,
+FOREIGN KEY(productID) REFERENCES Products(id)
+);
+
+
+CREATE TABLE SeasonPasses(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name varchar(255) NOT NULL DEFAULT 'na',
+startDate DATE NOT NULL,
+endDate DATE NOT NULL,
+cost float NOT NULL DEFAULT '0.0',
+productID INT NOT NULL,
+FOREIGN KEY(productID) REFERENCES Products(id)
+);
+
+CREATE TABLE Countries(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL DEFAULT 'na'
+);
+
+INSERT INTO Countries(name)
+VALUES ('USA'),('Canada'),('Mexico');
+
+
+
+CREATE TABLE StateCountries(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(5) NOT NULL DEFAULT 'na',
+fullName VARCHAR(255) NOT NULL DEFAULT 'na',
+countryID INT NOT NULL,
+FOREIGN KEY(countryID) REFERENCES Countries(id)
+);
+
+CREATE TABLE Addresses(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+street varchar(255) NOT NULL DEFAULT 'na',
+city varchar(255) NOT NULL DEFAULT 'na',
+stateCountryID INT NOT NULL,
+zipcode varchar(255) NOT NULL DEFAULT 'na',
+FOREIGN KEY(stateCountryID) REFERENCES StateCountries(id)
+);
+
+
+CREATE TABLE MovieTickets(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+dateTime datetime NOT NULL,
+movieName VARCHAR(255) NOT NULL,
+addressID INT NOT NULL,
+screenNumber VARCHAR(255) NOT NULL,
+pricePerUnit FLOAT NOT NULL DEFAULT '0.0',
+productID INT NOT NULL,
+FOREIGN KEY(productID) REFERENCES Products(id),
+FOREIGN KEY(addressID) REFERENCES Addresses(id)
+);
+
+CREATE TABLE Persons(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+personCode varchar(255) UNIQUE NOT NULL,
+lastName varchar(255) NOT NULL,
+firstName varchar(255) NOT NULL,
+addressID INT NOT NULL,
+foreign key(addressID) references Addresses(id)
+);
+
+
+create table Emails(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+personID INT NOT NULL,
+email 	VARCHAR(255) NOT NULL,
+FOREIGN KEY(personID) REFERENCES Persons(id)
+);
+
+CREATE TABLE Customers(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+customerCode VARCHAR(255) UNIQUE NOT NULL,
+customerName VARCHAR(255) NOT NULL,
+customerType VARCHAR(255) NOT NULL DEFAULT 'na',
+primaryContactID INT NOT NULL,
+addressID INT NOT NULL,
+FOREIGN KEY(primaryContactID) REFERENCES Persons(id),
+FOREIGN KEY(AddressID) REFERENCES Addresses(id)
+);
+
+
+
+CREATE TABLE Invoices(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+invoiceCode VARCHAR(255) UNIQUE NOT NULL,
+customerID INT NOT NULL DEFAULT 0,
+salesPersonID INT NOT NULL DEFAULT 0,
+invoiceDate DATE NOT NULL,
+FOREIGN KEY(CustomerID) REFERENCES Customers(id),
+FOREIGN KEY(salesPersonID) REFERENCES Persons(id)
+);
+
+CREATE TABLE InvoiceProducts(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+invoiceID INT NOT NULL ,
+productID INT NOT NULL,
+unit INT NOT NULL,
+note VARCHAR(255),
+FOREIGN KEY(invoiceID) REFERENCES Invoices(id),
+FOREIGN KEY(productID) REFERENCES Products(id),
+CONSTRAINT uniqueIP UNIQUE(invoiceID, productID)
+);
+
+
+
